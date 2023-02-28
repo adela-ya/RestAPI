b557339c63fa8d3d7b7e92f264412d2e146191c4
adela-ya <adelya.yarullina@metreshka.ru>
2023-02-28 10:58:49 +0300

    fix restApi

--- src/mvc/Controllers/Api/AuthController.php
+++ src/mvc/Controllers/Api/AuthController.php
@@ -11,20 +11,22 @@ use mvc\Models\UsersTokenModel;
 class AuthController extends ApiBaseController {
     public function loginAction(): JsonResponse {
         $postData = $this->getRequest()->post();
+        if (!empty($postData)) {
+            $usersModel = new UsersModel();
+            $userId     = $usersModel->checkInDb($postData);
+            if ($userId !== null) {
+                $usersTokenModel = new UsersTokenModel();
+                $getToken        = $usersTokenModel->checkTokenInDb($userId);
 
-        $usersModel = new UsersModel();
-        $userId     = $usersModel->checkInDb($postData);
-        if ($userId !== null) {
-            $usersTokenModel = new UsersTokenModel();
-            $getToken        = $usersTokenModel->checkTokenInDb($userId);
-
-            if (!$getToken) {
-                $postData['token'] = $usersTokenModel->addTokenToDb($userId);
-            } else {
-                $postData['token'] = $getToken['token'];
+                if (!$getToken) {
+                    $postData['token'] = $usersTokenModel->addTokenToDb($userId);
+                } else {
+                    $postData['token'] = $getToken['token'];
+                }
+                return $this->response($postData);
             }
-            return $this->response($postData);
         }
+
         $errors["code"] = '403';
         $errors["message"] = 'Неверный логин или пароль';
         return $this->responseError($errors);
